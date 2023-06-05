c13760599a8226a0ca1dadcd46e5b2a5d84d6292
Joshua Okero <102040133+okero-joshua@users.noreply.github.com>
2023-06-05 12:53:53 -0700

    Delete showJoshuaOkero.js

--- Show/showJoshuaOkero.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( () => {
-    $("#jdom a").click( evt => {
-    evt.preventDefault();
-    const a = evt.currentTarget;
-    const divID = $(a).attr("href");
-    $(divID).toggleClass();
-    $(divID).attr("class")=="hide" ? $(a).text("Show Less"): $(a).text("Show More")
-});
-});
